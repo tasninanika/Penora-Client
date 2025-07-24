684c03d667f1f0b60b53a13d97534a714dcec033
anikatasnin <jarintasnin09@gmail.com>
2025-07-24 22:23:36 +0600

    apply style on navbar

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-base-100">
+      <Navbar></Navbar>
       <main className="flex-1 container mx-auto px-4 py-6">
         <Outlet />
       </main>
+      <Footer></Footer>
     </div>
   );
 };
