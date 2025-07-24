fc6115c4963d0568b99c66765180c3b0fda4bee9
anikatasnin <jarintasnin09@gmail.com>
2025-07-24 23:48:09 +0600

    import style.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes";
 import "./index.css";
-
+import "./assets/styles/style.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
