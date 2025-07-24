c1795f9c4b42b53a48ca4e731596704cd47e37c9
anikatasnin <jarintasnin09@gmail.com>
2025-07-24 22:20:12 +0600

    using router provider

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-base-100">
-      <Navbar />
       <main className="flex-1 container mx-auto px-4 py-6">
         <Outlet />
       </main>
-      <Footer />
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import Routes from "./routes/Routes";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routes";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
